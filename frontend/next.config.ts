4e9054552e3040a80822719ce4f4327221345ee9
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2026-09-20 03:41:25 +0530

    Added Frontend CI/CD

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
-
+const backendUrl = process.env.BACKEND_URL || "http://localhost:8080";
 const nextConfig: NextConfig = {
   /* config options here */
+
     allowedDevOrigins: ['192.168.0.152'],
 
     async rewrites() {
         return [
             {
                 source: '/api/yjs/:path*',
-                destination: 'http://localhost:8080/api/yjs/:path*',
+                destination: `${backendUrl}/api/yjs/:path*`,
             },
         ];
     },
