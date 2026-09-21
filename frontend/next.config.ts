532d7a33cacff6720c4123c91d405c3b1547b59d
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2026-09-21 12:36:32 +0530

    Update bug forgot `--push` tag smh

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const backendUrl = process.env.BACKEND_URL || "http://localhost:8080";
+const backendUrl = process.env.BACKEND_URL;
 const nextConfig: NextConfig = {
   /* config options here */
     output: "standalone",
